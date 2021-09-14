52f490d7101da09671759f34e60c1addcb649a45
andriy-lanyk <LAM1988@ukr.net>
2021-09-14 14:25:29 +0300

    Delete json-server and add new store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from "./Contacts/contacts-reduser";
+import logger from "redux-logger";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import contactsReducer from "./Contacts/contacts-reducer";
+import authReducer from "./Auth/auth.reducer";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
 const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
+  devTools: process.env.NODE_ENV === "development",
 });
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./Redux/store";
 import App from "./Components/App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
